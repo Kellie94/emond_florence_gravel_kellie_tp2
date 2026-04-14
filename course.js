2e72e31ace122afcb650b7504776e1f873e2ac9e
Florence E <2443902@cmontmorency.qc.ca>
2026-04-13 23:02:46 -0400

    Update course.js

--- course.js
+++ course.js
@@ -1,3 +1,19 @@
+/* Variables */ 
+let car1 = document.getElementsByClassName("car1");
+let car2 = document.getElementsByClassName("car2");
+
+let translationX1 = 0;
+let translationX2 = 0;
+
+let selecteurContainerWidth = document.querySelector(".mains-car");
+let containerWidth = getComputedStyle(myDiv).getPropertyValue("width");    //Met valeur de la largeur de l'élément dans une variable
+let selecteurVoiture1Width = document.querySelector(".car1");
+let voiture1Width = getComputedStyle(myDiv).getPropertyValue("width");
+let selecteurVoiture2Width = document.querySelector(".car2");
+let voiture2Width = getComputedStyle(myDiv).getPropertyValue("width");
+
+
+
 function compteARebour() {
     let seconde = 6;
     let compteARebours = document.getElementById("compteARebours");
@@ -19,8 +35,12 @@ function chronometre() {
 }
 
 
-function aleatoireTranslation() {
-
+function aleatoireTranslationX() {
+    let maxTranslationX = containerWidth - voiture1Width;
+    let valeurAleatoire = Math.random() * (maxTranslationX + 1);
+    valeurAleatoire = valeurAleatoire * maxTranslationX;
+    valeurAleatoire = Math.floor(valeurAleatoire);
+    alert(valeurAleatoire);
 }
 
 
@@ -30,5 +50,6 @@ function deplacerVoiture() {
 
 
 function demarrerCourse() {
-
+    compteARebour();
+    
 }
